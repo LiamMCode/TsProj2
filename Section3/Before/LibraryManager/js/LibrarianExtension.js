a01f1e529b27ceedc1c3dadd86b3a47711e9d217
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-06-15 08:27:19 +0100

    Delete Section3/Before/LibraryManager directory

--- Section3/Before/LibraryManager/js/LibrarianExtension.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const classes_1 = require('./classes');
-classes_1.UniversityLibrarian.prototype.hostSeminar = function (topic) {
-    console.log('Hosting a seminar on ' + topic);
-};
-//# sourceMappingURL=LibrarianExtension.js.map
