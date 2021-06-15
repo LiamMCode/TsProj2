ecc986a36b73719dd787b1da73cb492ab9af9ccd
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-06-15 08:26:37 +0100

    Delete Section5/Before directory

--- Section5/Before/LibraryManager/app.ts
+++
@@ -1,5 +0,0 @@
-import { Category } from './enums';
-import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
-import { PublicLibrarian, UniversityLibrarian, ReferenceItem, Employee, Researcher } from './classes';
-import * as util from './lib/utilityFunctions';
-
